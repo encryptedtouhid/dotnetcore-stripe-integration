ded1cf3afe435b75529ef62ff9b16e528d0942af
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2025-04-27 20:19:24 +0800

    redesign card added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Pages/**/*.cshtml",
+    "./Pages/**/*.js",
+    "./wwwroot/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
